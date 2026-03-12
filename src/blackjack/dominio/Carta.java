ef07082ef873784de553842c326187b7f98d707d
Raquel Gonzalez Carrillo <rgoncar723@g.educaand.es>
2026-03-12 23:05:58 +0100

    Refactor Carta class with constructor and methods

--- src/blackjack/dominio/Carta.java
+++ src/blackjack/dominio/Carta.java
@@ -1,9 +1,23 @@
 package blackjack.dominio;
 
 public class Carta {
-
-	private TipoCarta tipo;
-	private Palo palo;
+	private TipoCarta carta;
+	private TipoPalo palo;
 	
-	
-}
+	public Carta(TipoCarta carta, TipoPalo palo) {
+		this.carta=carta;
+		this.palo=palo;
+		}
+	public int getValorNumerico() {
+		return carta.getValor();
+	}
+	public String getPalo() {
+		return palo.getDescription();
+	}
+	public boolean esAs() {
+		return carta.esAs();
+	}
+	@Override
+	public String toString() {
+		return String.format("%d%s", getValorNumerico(), getPalo());
+	}
