1ea486c72ab138a06a68e5f23684c2deebbb2bbb
Raquel Gonzalez Carrillo <rgoncar723@g.educaand.es>
2026-03-13 21:43:31 +0100

    Enhance Palo enum with documentation comments
    Added documentation comments for the Palo enum and its constructor.

--- src/blackjack/dominio/Palo.java
+++ src/blackjack/dominio/Palo.java
@@ -1,15 +1,27 @@
 package blackjack.dominio;
 
 public enum Palo {
+	/**
+	 * Representa los cuatro palos de la baraja.
+	 * Cada palo tiene asociado un símbolo gráfico para su representación visual 
+	 * en la interfaz de consola.
+	 * * @author RGONCAR723
+	 * 
+	 */
+	CORAZONES("❤️"), PICAS("♠️"), DIAMANTES("♦️"), TREBOLES("♣️");
 
-	CORAZONES("❤️"),PICAS("♠️"),DIAMANTES("♦️"),TREBOLES("♣️");
-	
 	private String description;
-	
-	Palo(String description){
-		this.description=description;
+	/**
+     * Constructor del enumerado Palo.
+     * @param description Cadena de texto que contiene el símbolo del palo.
+     */
+	Palo(String description) {
+		this.description = description;
 	}
-	
+	/**
+     * Obtiene el símbolo gráfico asociado al palo.
+     * @return El carácter o emoji que representa al palo.
+     */
 	public String getDescription() {
 		return description;
 	}
